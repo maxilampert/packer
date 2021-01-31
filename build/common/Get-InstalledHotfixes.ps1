7f0b84569aba64043998b4ad08fa0d43a1b04f13
Aaron Parker <aaron@stealthpuppy.com>
2021-02-01 08:38:09 +1100

    Software/Hotfixes as seperate files

---
+++ build/common/Get-InstalledHotfixes.ps1
@@ -0,0 +1,39 @@
+<#
+#>
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [System.String] $HotfixFile = "$env:SystemRoot\Temp\InstalledHotfix.json"
+)
+
+#region Functions
+Function Get-InstalledHotfixes {
+    <#
+        .SYNOPSIS
+            Retrieves a list of hotfixes installed
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding()]
+    param ()
+
+    try {
+        $HotfixList = Get-Hotfix | Select-Object -Property "Description", "HotFixID", "Caption" | Sort-Object -Property "HotFixID"
+    }
+    catch {
+        Throw $_
+    }
+    
+    try {
+        $HotfixJson = $HotfixList | ConvertTo-Json
+    }
+    catch {
+        Throw $_
+    }
+
+    If ($Null -ne $HotfixJson) { Write-Output -InputObject $HotfixJson }
+}
+#endregion
+
+# Output the hotfix list to a JSON file that Packer can upload back to the runner
+Write-Host "================ Export hotfix list to: $HotfixFile."
+Get-InstalledHotfixes | Out-File -FilePath $HotfixFile -Force -Encoding "Utf8"
--- build/common/Get-InstalledSoftware.ps1
+++ build/common/Get-InstalledSoftware.ps1
@@ -3,9 +3,7 @@
 [CmdletBinding()]
 param (
     [Parameter()]
-    [System.String] $SoftwareFile = "$env:SystemRoot\Temp\InstalledSoftware.json",
-
-    [System.String] $HotfixFile = "$env:SystemRoot\Temp\InstalledHotfix.json"
+    [System.String] $SoftwareFile = "$env:SystemRoot\Temp\InstalledSoftware.json"
 )
 
 #region Functions
@@ -62,45 +60,12 @@ Function Get-InstalledSoftware {
         }
     }
 }
-
-Function Get-InstalledHotfixes {
-    <#
-        .SYNOPSIS
-            Retrieves a list of hotfixes installed
-    #>
-    [OutputType([System.Management.Automation.PSObject])]
-    [CmdletBinding()]
-    param ()
-
-    try {
-        $HotfixList = Get-Hotfix | Select-Object -Property "description", "hotfixid" , "caption" | Sort-Object -Property "HotFixID"
-    }
-    catch {
-        Throw $_
-    }
-    
-    try {
-        $HotfixJson = $HotfixList | ConvertTo-Json
-    }
-    catch {
-        Throw $_
-    }
-
-    If ($Null -ne $HotfixJson) { Write-Output -InputObject $HotfixJson }
-}
 #endregion
-
-# Output the hotfix list to a JSON file that Packer can upload back to the runner
-Write-Host "================ Export hotfix list to: $HotfixFile."
-Get-InstalledHotfixes | Out-File -FilePath $HotfixFile -Force -Encoding "Utf8"
-
-# Get the installed software list
-$software = Get-InstalledSoftware
 
 # Output the software list to a JSON file that Packer can upload back to the runner
 Write-Host "================ Export software list to: $SoftwareFile."
+$software = Get-InstalledSoftware
 $software | ConvertTo-Json | Out-File -FilePath $SoftwareFile -Force -Encoding "Utf8"
 
 # Output the installed software to the pipeline for Packer output
-$software = Get-InstalledSoftware
-$software | Sort-Object -Property Publisher, Version
+Return (Get-InstalledSoftware | Sort-Object -Property "Publisher", "Version")
