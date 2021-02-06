06737319a95fd7ad91e88f42b2de5d1187937d13
Aaron Parker <aaron@stealthpuppy.com>
2021-02-06 20:32:36 +1100

    Fix markdown; update markdown output

--- bin/Publish-MarkdownReport.ps1
+++ bin/Publish-MarkdownReport.ps1
@@ -34,9 +34,9 @@ Write-Host "================ ImageOffer:        $ImageOffer."
 Write-Host "================ ImageSku:          $ImageSku."
 Write-Host "================ DestinationPath:   $ImagePublisher."
 
-# Start with a blank markdown variable
-[System.String] $markdown
-$markdown += New-MDHeader -Text $version -Level 1
+# Start with a markdown variable
+[System.String] $markdown += New-MDHeader -Text $version -Level 1 -NoNewLine
+$markdown += "`n"
 
 # Read the contents of the output files, convert to markdown
 ForEach ($file in $InputFile) {
@@ -52,10 +52,11 @@ ForEach ($file in $InputFile) {
         }
 
         If ($table) {
-            $markdown += New-MDHeader -Text ($file -replace ".json", "") -Level 2
+            $markdown += New-MDHeader -Text ($file -replace ".json", "") -Level 2 -NoNewLine
+            $markdown += "`n"
             $markdown += $table | Sort-Object -Property "Publisher", "Name", "Version" | New-MDTable
-            $markdown += ""
-            Remove-Variable -Name "table"
+            $markdown += "`n"
+            Remove-Variable -Name "table" -ErrorAction "SilentlyContinue"
         }
     }
     Else {
@@ -76,9 +77,10 @@ catch {
 # Write the markdown to a file
 try {
     Write-Host "================ Writing markdown to: $(Join-Path -Path $TargetPath -ChildPath "$Version.md")."
-    $markdown | Out-File -FilePath (Join-Path -Path $TargetPath -ChildPath "$Version.md") -Encoding "Utf8" -Force
+    If ($markdown[-1] -ne "`n") { $markdown += "`n" }
+    $markdown | Out-File -FilePath (Join-Path -Path $TargetPath -ChildPath "$Version.md") -Encoding "Utf8" -NoNewLine -Force
 }
-catch {
+catch {s
     Throw $_
     Break
 }
