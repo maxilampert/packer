e33cb36b48c6131bb3038714e2808a9875484f92
Aaron Parker <aaron@stealthpuppy.com>
2021-12-06 22:51:58 +1100

    Update Get-PackerPlugins.ps1

--- pipeline/Get-PackerPlugins.ps1
+++ pipeline/Get-PackerPlugins.ps1
@@ -126,6 +126,10 @@ $Latest = [PSCustomObject]@{
     Version = "0.14.0"
     URI     = "https://github.com/rgl/packer-plugin-windows-update/releases/download/v0.14.0/packer-plugin-windows-update_v0.14.0_x5.0_windows_amd64.zip"
 }
+$Latest = [PSCustomObject]@{
+    Version = "0.11.0"
+    URI     = "https://github.com/rgl/packer-plugin-windows-update/releases/download/v0.11.0/packer-provisioner-windows-update_0.11.0_windows_amd64.zip"
+}
 
 If ($Null -ne $Latest) {
     Write-Host " Found version: $($Latest.Version)."
