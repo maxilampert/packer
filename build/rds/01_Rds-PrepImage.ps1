059d814db431853a31643803fab684feb1d6ff08
Aaron Parker <aaron@stealthpuppy.com>
2021-12-26 22:08:42 +1100

    Script updates

--- build/rds/01_Rds-PrepImage.ps1
+++ build/rds/01_Rds-PrepImage.ps1
@@ -4,17 +4,11 @@
 #>
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
 [CmdletBinding()]
-Param (
-    [Parameter(Mandatory = $False)]
-    [System.String] $Path = "$env:SystemDrive\Apps\Prep"
-)
+Param ()
 
 # Ready image
 Write-Output " Disable Windows Defender real time scan"
 Set-MpPreference -DisableRealtimeMonitoring $true
-
-# Create target folder
-New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" > $Null
 
 Write-Output " Disable Windows Store updates"
 REG add HKLM\Software\Policies\Microsoft\Windows\CloudContent /v "DisableWindowsConsumerFeatures" /d 1 /t "REG_DWORD" /f
